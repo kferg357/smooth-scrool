b62c529d99df78a508f6b0c4d6868737c2c59baf
kferg357 <kennethferguson90@gmail.com>
2022-01-31 13:02:51 -0500

    added app.js amd mre html

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const menu =document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
 //Display Mobile Menu
-const MobileMenu = () => {
+const mobileMenu = () => {
   menu.classList.toggle('is-active');
-  menuLinks.class.toggle('active');
+  menuLinks.classList.toggle('active');
 };
 
-menu.addEventListener('click', MobileMenu);
+menu.addEventListener('click', mobileMenu);
